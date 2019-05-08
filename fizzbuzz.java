c6195b680e1d417a83c42948a2de98aa0c1ab46b
hasanalattar <50388769+hasanalattar@users.noreply.github.com>
2019-05-08 11:00:18 +0800

    Update fizzbuzz.java

--- fizzbuzz.java
+++ fizzbuzz.java
@@ -2,7 +2,7 @@
  
 public class FizzBuzz
 {
-    public static void main(String[] args)// definging the public class
+    public static void main(String[] args)// defining the public class
     {
         fizzBuzz1(100); //Defining the counter
         }
@@ -12,13 +12,13 @@ public class FizzBuzz
         for (int i = 1; i <= num; i++)//the condition of starting with No 1, add 1, to reach 100
         {
             if (((i % 5) == 0) && ((i % 3) == 0)) // Is it a multiple of 5 & 3?
-                System.out.println("fizzbuzz");// if it is not multiple of 5 and 3
-            else if ((i % 3) == 0) // Is it a multiple of 3?
+                System.out.println("fizzbuzz");// print fizzbuzz
+            else if ((i % 3) == 0) // Is it a multiple of 3 condition? Print fizz
                 System.out.println("fizz");
-            else if ((i % 5) == 0) // Is it a multiple of 5?
+            else if ((i % 5) == 0) // Is it a multiple of 5 condition? print buzz
                 System.out.println("buzz");
             else
-                System.out.println(i); // Not a multiple of 5 or 3
+                System.out.println(i); // Not a multiple of 5 or 3 condition, print number
         }
     }
  
